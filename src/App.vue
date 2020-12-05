46b15e6857e9bffee266f507d245a009ca4585e1
hujinming <hujinming@shenguyun.com>
2020-12-05 14:44:55 +0800

    vue-big-table 0.1.0

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app">
+    <vue-big-table :columns='columns' :tableList='dataTable' :tdHeight="30" :tableBodyHeight="600"></vue-big-table>
+  </div>
+</template>
+
+<script>
+import vueBigTable from './components/vue-big-table'
+
+export default {
+  name: 'app',
+  components: {
+    vueBigTable
+  },
+  data(){
+    return {
+      columns: [
+        //表头
+        {
+          title: "列1",
+          key: "columns1",
+          width: null,
+          align: "center",
+        },
+        {
+          title: "列2",
+          key: "columns2",
+          width: null,
+          align: "center"
+        },
+        {
+          title: "列3",
+          key: "columns3",
+          width: null,
+          align: "center"
+        },
+        {
+          title: "列4",
+          key: "columns4",
+          width: null,
+          align: "center"
+        },
+        {
+          title: "列5",
+          key: "columns5",
+          width: null,
+          align: "center"
+        },
+        {
+          title: "列6",
+          key: "columns6",
+          width: null,
+          align: "center"
+        },
+        {
+          title: "列7",
+          key: "columns7",
+          width: null,
+          align: "center"
+        }
+      ],
+      dataTable: [] //表格数据
+    }
+  },
+  mounted() {
+    for (let i = 0; i < 200000; i++) {
+      let data = {};
+      for (let m = 0; m < this.columns.length; m++) {
+        data[`columns${m}`] = i; //`${i+1}行${m+1}列`;
+      }
+      this.dataTable.push(data);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
